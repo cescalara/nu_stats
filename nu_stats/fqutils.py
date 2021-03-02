ad249c585068a835f32dc7bc2d530982a3b18121
Vminoz <minoz.valentin@gmail.com>
2021-03-02 17:52:41 +0100

    minor updates, fq still wip

--- nu_stats/fqutils.py
+++ nu_stats/fqutils.py
@@ -1,15 +1,10 @@
 import numpy as np
-from astropy.coordinates import SkyCoord
-from .plotting import SphericalCircle
-from matplotlib import pyplot as plt
-from astropy import units as u
 
 
 def omega_s(t: float):
     """ Describes the model-dependent relation between
         the γ-ray emission and the expected neutrino flux
         from source s as a function of time
-
     Args:
         t (float): time
     """
@@ -69,48 +64,3 @@ def test_stat(source_dir: np.ndarray, obs_dir: np.ndarray, kappa: float):
             S += source_likelihood(source_dir[i:i+1,:], obs_dir[j:j+1,:], kappa)
         TS[j] = 2*np.log(S / bg_likelihood(obs_dir[j:j+1,:]))
     return TS
-
-
-def unit_vectors_skymap(unit_vector_dir: np.ndarray, labels: np.ndarray = None):
-    if labels is not None:
-        assert isinstance(labels, np.ndarray)
-        assert labels.shape[0] == unit_vector_dir.shape[0]
-    coords = SkyCoord(
-            unit_vector_dir.T[0],
-            unit_vector_dir.T[1],
-            unit_vector_dir.T[2],
-            representation_type="cartesian",
-            frame="icrs",
-        )
-    coords.representation_type = "spherical"
-
-    fig, ax = plt.subplots(subplot_kw={"projection": "astro degrees mollweide"})
-    fig.set_size_inches((7, 5))
-    if labels is None:
-        for ra, dec in zip(
-            coords.icrs.ra,
-            coords.icrs.dec,
-        ):
-            circle = SphericalCircle(
-                (ra, dec),
-                3 * u.deg,
-                alpha=0.5,
-                transform=ax.get_transform("icrs"),
-            )
-            ax.add_patch(circle)
-    else:
-        label_cmap = plt.cm.tab10(list(range(2)))
-        for ra, dec, l in zip(
-            coords.icrs.ra,
-            coords.icrs.dec,
-            labels,
-        ):
-            circle = SphericalCircle(
-                (ra, dec),
-                3 * u.deg,
-                color=label_cmap[l],
-                alpha=0.5,
-                transform=ax.get_transform("icrs"),
-            )
-            ax.add_patch(circle)
-    return coords, labels
